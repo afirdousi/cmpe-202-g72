96f5a31c8a3365d97891444f2e1efe573537f4df
anasrazafirdousi <anasrazafirdousi@172.31.99.244>
2016-08-05 21:10:57 -0700

    Basic member class setup with CRUD methods

--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
 	String  name;
 	String  email;
@@ -7,28 +10,75 @@ public class Member {
 	String  dob;
 	String  licenceNumber;
 	String 	paymentDetails;
-	String 	Id;
+	String 	ID;
 	boolean isPremiumMember;
+	
+	List<Member> members = new ArrayList<Member>();
 
-	public void CreateMember(){}
+	public void createMember(Member m){
+		this.members.add(m);
+	}
 	
-	public void retriveMember(){}
+	//Check for ID null when you retrieve member
+	public Member searchMember(){
+		
+		Member result = new Member();
+		
+		for(Member member : members){
+			if(member.ID==ID){
+				result = member;
+				break;
+			}
+		}
+		
+		return result;
+	}
 	
-	public void updateMember(){}
+	public void updateMember(Member m){
+		for(Member member : members){
+			if(m.ID==ID){
+				member.name = m.name;
+				member.email = m.email;
+				member.phoneNumber = m.phoneNumber;
+				member.address = m.address;
+				member.password = m.password;
+				member.dob = m.dob;
+				member.licenceNumber = m.licenceNumber;
+				member.paymentDetails = m.paymentDetails;
+				member.isPremiumMember = m.isPremiumMember;
+				
+				break;
+			}
+		}
+		
+	}
 	
-	public void deleteMember(String Id){}
+	public void deleteMember(String ID){
+		for(Member m : members){
+			if(m.ID==ID){
+				this.members.remove(m);
+				
+				break;
+			}
+		}
+
+	}
 	
-	public void selectMember(String Id){}
-	
-	public void searchMember(String Id){}
 	
 	public void sortMember(){}	//ascending order
 	//Will add all getter and setter for this class  
 	
-	public void setMemberName(String name){}
+	public void setMemberName(String name){
+		this.name = name;
+		
+	}
 	
-	public void getMemberName(){}
+	public String getMemberName(){
+		return this.name;
+	}
 	
-	public void setMemberEmail(String email){}
+	public void setMemberEmail(String email){
+		this.email = email;
+	}
 	
 }
