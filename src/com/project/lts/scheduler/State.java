84c21eab0cacf203f22d63c7dfd111c002a1f9bb
srinathsj92 <srinathsj1992@gmail.com>
2016-08-09 23:37:01 -0700

    Scheduler module commit

--- src/com/project/lts/scheduler/State.java
+++
@@ -1,9 +0,0 @@
-package com.project.lts.scheduler;
-public interface State {
-
-	public String receiveRequest(boolean x);
-	
-	public String calculateRide(boolean y);
-	
-	public String dispatchRide(boolean z);
-}
