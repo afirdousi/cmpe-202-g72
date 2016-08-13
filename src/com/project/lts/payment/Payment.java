b7c7d1952231ff5e5af2be5cce859c1369c3c6ad
snehalgolhar <snehal.golhar89@gmail.com>
2016-08-13 09:35:19 -0700

    Added Payment Code ----Snehal

--- src/com/project/lts/payment/Payment.java
+++ src/com/project/lts/payment/Payment.java
@@ -1,20 +1,83 @@
 package com.project.lts.payment;
-public class Payment {
-	
-	//ScheduledRide rideDetails;//to get ride information
-	String amount;
-	String coupon;
-	String paymentConfirmationNumber;
-	String payment;
 
-	public void computePayment(/*memberType,coupon,amount*/){}
-	
-	public void collectPayment(){}
-	
-	public void notifyCustomer(){}
-	
-	public void processPayment(/*paymentDetails,paymentConfirmationNumber*/){}
-	
-	public void showPaymentHistory(/*memberId,paymentConfirmationNumber*/){}
+public class Payment {
+	//ScheduledRide rideDetails;//to get ride information
+		int rideAmount;
+		String coupon;
+		String paymentConfirmationNumber;
+		String payment;
+		String paymentMode;
+		String memberType="";
+		boolean isCoupon;
+		
+		
+		
+		public void sendPayment(int amount){			
+			
+			rideAmount=amount;						
+			if(paymentMode=="paypal")
+			{
+			  
+		      pay (new PaypalStrategy("myemail@example.com", "mypwd"));
+			}
+			else
+			{
+				
+			     pay (new CreditCardStrategy("Snehal Golhar", "123466778844334", "996", "2/20"));
+
+			}
+		}
+
+		public int computePayment(){
+				
+			/* need 
+				 * memberType,coupon,amount,memberobject*/
+			if(memberType=="Premium"){
+				rideAmount=rideAmount-10;	
+				
+			}
+			if(isCoupon==true){
+				rideAmount=rideAmount-5;
+			}
+			
+			return rideAmount;
+				
+			
+		}
+		
+		public void pay(PaymentStrategy paymentMethod){
+			
+			
+		    int amount = computePayment();			
+			paymentMethod.pay(amount);
+			//processPayment();
+		
+	   }
+		
+		public void holdPayment(int amount){
+			System.out.println("Payment"+amount+" is hold for ride ");
+		}
+		
+		public void notifyCustomer(){
+			//code to notify user
+		}
+		
+		public void processPayment(/*paymentDetails,paymentConfirmationNumber*/){
+			if(paymentMode=="paypal")
+			{
+			  
+		      pay (new PaypalStrategy("myemail@example.com", "mypwd"));
+			}
+			else
+			{
+				
+			     pay (new CreditCardStrategy("Snehal Golhar", "123466778844334", "996", "2/20"));
+
+			}
+		}
+		
+		public void showPaymentHistory(/*memberId,paymentConfirmationNumber*/){
+			
+		}
 
 }
---
+++ src/com/project/lts/payment/PaymentStrategy.java
@@ -0,0 +1,6 @@
+package com.project.lts.payment;
+
+public interface PaymentStrategy {
+	public void pay(int amount);
+
+}
