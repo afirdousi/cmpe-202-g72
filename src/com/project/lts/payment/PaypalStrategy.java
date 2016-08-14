8676ee0c713e49c69c5c53fcb071a57d15da34aa
snehalgolhar <snehal.golhar89@gmail.com>
2016-08-13 22:35:24 -0700

    payment related changes--Snehal

--- src/com/project/lts/payment/PaypalStrategy.java
+++ src/com/project/lts/payment/PaypalStrategy.java
@@ -1,21 +1,28 @@
 package com.project.lts.payment;
+import com.project.lts.accounts.Member;
+import com.project.lts.notification.Notification;
 
+//Changed 8/13/2016
 public class PaypalStrategy implements PaymentStrategy {
 	private String emailId;
 	private String password;
+	private Member member;
+	Notification notificationManager=new Notification();
 	
-	public PaypalStrategy(String email, String pwd){
+	public PaypalStrategy(Member member,String email, String pwd){
 		this.emailId=email;
 		this.password=pwd;
+		this.member=member;
 	}
 	
 	@Override
 	public void pay(int amount) {
-		/*String message=amount +" paid using Paypal";
-		Notification n=new Notification();
-		n.setMessage(message);
-		n.send();*/
-		System.out.println(amount + " paid using Paypal.");
+		
+		notificationManager.reset();
+		notificationManager.setListener(member);
+		notificationManager.setMessage(amount +" paid with Paypal");
+		notificationManager.send();
+		//System.out.println(member.getMemFname()+amount + " paid using Paypal111.");
 	}
 
 }
