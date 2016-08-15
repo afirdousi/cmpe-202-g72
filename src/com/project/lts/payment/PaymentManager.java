d65e42f08b9d9943367245ec1e2ba2d16f899f50
snehalgolhar <snehal.golhar89@gmail.com>
2016-08-14 22:47:35 -0700

    Added send notification method to in PaymentManager.java --Snehal

--- src/com/project/lts/payment/PaymentManager.java
+++ src/com/project/lts/payment/PaymentManager.java
@@ -3,11 +3,15 @@ package com.project.lts.payment;
 import java.util.List;
 
 import com.project.lts.accounts.Member;
+import com.project.lts.notification.Notification;
 
 public class PaymentManager {
 	
+	Notification notificationManager=new Notification();
+	
 	public void proceesPayment(int rideAmount,List<Member> members){
 		Payment objP=new Payment();	
+		
 		
 		for (int i = 0; i < members.size(); i++)
 		{
@@ -37,15 +41,18 @@ public class PaymentManager {
 			
 		}
 		
+		//Code for holding payment
 		public void holdPayment(int amount,List<Member> members){
-			setRideAmount(amount);
+	
 			for (int i = 0; i < members.size(); i++)
 			{
 				notificationManager.reset();
 				notificationManager.setListener(members.get(i));
 				notificationManager.setMessage("Hello"+members.get(i).getMemFname()+" Payment"+ amount + "is hold for ride ");
-			   //System.out.println("Hello"+members.get(i).getMemFname()+" Payment"+ amount + "is hold for ride ");
-		     }
+			    notificationManager.send();
+				//System.out.println("Hello"+members.get(i).getMemFname()+" Payment"+ amount + "is hold for ride ");
+		     
+			}
 		}
 		
 		
